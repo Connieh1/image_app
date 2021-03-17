6b8d8ce15e2a1923c657648597918e24456faeb4
Connie Hyman <connie2630a@gmail.com>
2021-03-17 00:37:23 -0700

    Build out the card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,33 @@
 import React from "react";
 
 const Card = ({ images }) => {
-  return <>{images.map((image) => console.log(image))}</>;
+  return (
+    <>
+      {images.map((image) => (
+        <div key={image.id} className="card">
+          <a
+            href={image.largeImageURL}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={image.webformatURL}
+              alt="photograph"
+              className="cardImage"
+            />
+            <div className="cardInfo">
+              <p className="user">Photo by {image.user}</p>
+              <p>
+                <i className="fas fa-eye"></i>
+                <span>Views:</span>
+                {image.views}
+              </p>
+            </div>
+          </a>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default Card;
